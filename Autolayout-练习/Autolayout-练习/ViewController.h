ff74e16e898670b30c3024062db6a723ed06ef11
赵鹏 <zhaopeng@zhaopengdemacbook-pro.local>
2018-07-30 18:50:44 +0800

    创建项目。

---
+++ Autolayout-练习/Autolayout-练习/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Autolayout-练习
+//
+//  Created by apple on 16/1/25.
+//  Copyright © 2016年 sunco. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
---
+++ Autolayout-练习/Autolayout-练习/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Autolayout-练习
+//
+//  Created by apple on 16/1/25.
+//  Copyright © 2016年 sunco. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+#pragma mark ————— 生命周期 —————
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
